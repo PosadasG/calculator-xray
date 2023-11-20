2a096fce5dfa4c57da4a3375cabe9b1842050f27
PosadasG <84809997+PosadasG@users.noreply.github.com>
2023-11-19 22:50:40 -0500

    Update publish.bat

--- ci-scripts/publish.bat
+++ ci-scripts/publish.bat
@@ -1,4 +1,4 @@
 cd %~dp0
 cd ..
 cd nuget
-nuget push pdf.sharp-netstandard-mpl-agpl.4.2.0.nupkg -Source http://artifactory4.rnd.aspentech.com:8081/artifactory/api/nuget/devops-nuget-dev-local  -username admin -password "AKCp8nzqUqDp7pJzWDK5FTvXzc8fxRyu5XwEUPGngkF2o6CYgFEaZK8wM4yqy3naqYrXDDCyC"  
+nuget push pdf.sharp-netstandard-mpl-agpl.4.2.0.nupkg -Source http://server.com:8081/artifactory/api/nuget/devops-nuget-dev-local  -username admin -password "TOKEN"  
